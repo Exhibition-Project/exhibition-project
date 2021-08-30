347586046476e8b2b0378df79ee2d5d1a1cf2fb0
Changwan Chae <69083280+chaechangwan@users.noreply.github.com>
2021-08-30 15:45:38 +0900

    statistics 수정

--- src/kosta/mvc/model/dao/StatisticsDAO.java
+++ src/kosta/mvc/model/dao/StatisticsDAO.java
@@ -1,9 +1,10 @@
 package kosta.mvc.model.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import kosta.mvc.model.dto.StatisticsDTO;
 
 public interface StatisticsDAO {
-	List<StatisticsDTO> selectStatisticsByNo(int no, String firstDate, String lastDate);
+	List<StatisticsDTO> selectStatisticsByNo(int no, String firstDate, String lastDate) throws SQLException;
 }
