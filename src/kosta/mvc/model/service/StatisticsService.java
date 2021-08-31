ac224b7608ee49226844e97761e2c9f149f0452a
Changwan Chae <69083280+chaechangwan@users.noreply.github.com>
2021-08-31 10:27:33 +0900

    dbQuery order by 수정

--- src/kosta/mvc/model/service/StatisticsService.java
+++ src/kosta/mvc/model/service/StatisticsService.java
@@ -2,6 +2,7 @@ package kosta.mvc.model.service;
 
 import java.sql.SQLException;
 
+import kosta.mvc.exception.SearchNotFoundException;
 import kosta.mvc.model.dao.StatisticsDAO;
 import kosta.mvc.model.dao.StatisticsDAOImpl;
 import kosta.mvc.model.dto.ExhibitionDTO;
@@ -13,10 +14,10 @@ public class StatisticsService {
 	public ExhibitionDTO selectStatisticsByNo(int no, String firstDate, String lastDate) throws SQLException{
 		ExhibitionDTO exhibitionDTO = statisTicsDAO.selectStatisticsByNo(no, firstDate, lastDate);	
 		if(exhibitionDTO == null) {
-			throw new SQLException("찾으시는 전시회가 존재하지 않습니다.");
+			throw new SearchNotFoundException("찾으시는 전시회가 존재하지 않습니다.");
 		}
 		if(exhibitionDTO.getStatisticsList() == null ||  exhibitionDTO.getStatisticsList().size() == 0) {
-			throw new SQLException("찾으시는 기간의 예매 내역이 없습니다.");
+			throw new SearchNotFoundException("찾으시는 기간의 예매 내역이 없습니다.");
 		}
 		return exhibitionDTO;
 	}
